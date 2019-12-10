2888aad5c29c5e80d0f66fbf8588873a6dde8e02
Miron Banks <madapegarage@gmail.com>
2019-12-10 21:47:44 +0100

    list item styling

---
+++ src/index 2.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+ReactDOM.render(
+  //render element
+  <App />,
+  //target
+  document.getElementById('root')
+);
