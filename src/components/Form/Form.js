4e0118a21da6a46a7b58628c191cf129111f09da
Miron Banks <madapegarage@gmail.com>
2019-12-13 02:50:32 +0100

    cheanging app structure

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,39 +1,32 @@
 import React from "react";
-import "./Form.css";
+import styles from "./Form.module.scss";
+import Input from "../Input/Input";
+import Button from '../Button/Button';
+import Title from '../Title/Title';
 
 const Form = ({ submitFn }) => (
-  <div className="form__wrapper">
-    <h2>Add new twitter account</h2>
-    <form className="form__form" onSubmit={submitFn}>
-      <div className="form__item">
-        <input
-          type="text"
-          name="name"
-          id="name"
-          placeholder=" "
-          maxLength="30"
-        />
-        <label htmlFor="name">Name</label>
-        <div className="forms__item__bar" />
-      </div>
-      <div className="form__item">
-        <input type="text" name="link" id="link" placeholder=" " />
-        <label htmlFor="link">Link</label>
-        <div className="forms__item__bar" />
-      </div>
-      <div className="form__item">
-        <input type="text" name="image" id="image" placeholder=" " />
-        <label htmlFor="image">Image</label>
-        <div className="forms__item__bar" />
-      </div>
-      <div className="form__item">
-        <textarea name="description" id="description" placeholder=" " />
-        <label htmlFor="description">Description</label>
-        <div className="forms__item__bar" />
-      </div>
-      <button className="form__button" type="submit">
-        add new item
-      </button>
+  <div className={styles.wrapper}>
+    <Title>Add new twitter account</Title>
+    <form autoComplete="off" className={styles.form} onSubmit={submitFn}>
+      <Input
+        name="name"
+        label="Name"
+        maxLength={30}
+      />
+      <Input
+        name="link"
+        label="Twitter link"
+      />
+      <Input
+        name="image"
+        label="Image"
+      />
+      <Input
+        tag="textarea"
+        name="description"
+        label="Description"
+      />
+      <Button>add new item</Button>
     </form>
   </div>
 );
