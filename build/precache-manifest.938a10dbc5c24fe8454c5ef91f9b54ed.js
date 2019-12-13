8ce368f451d6a770a886076a970f790eda69948e
Miron Banks <madapegarage@gmail.com>
2019-12-13 07:24:46 +0100

    netlify

---
+++ build/precache-manifest.938a10dbc5c24fe8454c5ef91f9b54ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02996c93dbdfb1aaf4d7",
+    "url": "/static/css/main.9fe824b2.chunk.css"
+  },
+  {
+    "revision": "02996c93dbdfb1aaf4d7",
+    "url": "/static/js/main.02996c93.chunk.js"
+  },
+  {
+    "revision": "df87ea6d03a8e5978885",
+    "url": "/static/js/1.df87ea6d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a87d0f352b87d3736e3487e389f37ff7",
+    "url": "/static/media/logo.a87d0f35.svg"
+  },
+  {
+    "revision": "38be38cdb801a2a384db4c52ecfa8c0f",
+    "url": "/index.html"
+  }
+];
